9d923cfd6ce71451b95eab42e1d867190d769f05
Evgeny Kuizhev <ekuizhev@gmail.com>
2019-05-14 21:30:27 +0300

    add quadratic equation task

---
+++ Basic Ruby. Chapter 1/4.quadratic_equation.rb
@@ -0,0 +1,22 @@
+print "Enter a: "
+a = Float gets.chomp
+
+print "Enter b: "
+b = Float gets.chomp
+
+print "Enter c: "
+c = Float gets.chomp
+
+D = b**2 - 4*a*c
+
+if D > 0
+  x1 = (-b + Math.sqrt(D)) / (2.0*a)
+  x2 = (-b - Math.sqrt(D)) / (2.0*a)
+  puts "x1 = #{x1}"
+  puts "x2 = #{x2}"
+elsif D == 0
+  x = -b / (2.0*a)
+  puts "x = #{x}"
+else
+  puts "No roots!"
+end

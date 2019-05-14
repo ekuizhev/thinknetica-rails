1c2707d7391246a611ef1ff246a6582c3faa2426
Evgeny Kuizhev <ekuizhev@gmail.com>
2019-05-14 16:05:27 +0300

    add triangle square task

---
+++ Basic Ruby. Chapter 1/2.triangle_square.rb
@@ -0,0 +1,8 @@
+print "Enter base of a triangle: "
+a = Integer gets.chomp
+
+print "Enter height of a triangle: "
+h = Integer gets.chomp
+
+square = 0.5 * a * h;
+puts "Square of the triangle is #{square}"

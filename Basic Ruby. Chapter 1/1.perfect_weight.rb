fa89c7d7294b40804edd1c4b9cb7ecb3f56b9ccf
Evgeny Kuizhev <ekuizhev@gmail.com>
2019-05-14 15:18:34 +0300

    add perfect weight task

---
+++ Basic Ruby. Chapter 1/1.perfect_weight.rb
@@ -0,0 +1,13 @@
+print "Enter your name: "
+person_name = gets.chomp.capitalize
+
+print "Enter your height: "
+person_height = Integer gets.chomp
+
+perfect_weight = person_height - 110
+
+if perfect_weight > 0
+  puts "#{person_name}, your perfect weight is #{perfect_weight}!"
+else
+  puts "Your weight is already optimal!"
+end

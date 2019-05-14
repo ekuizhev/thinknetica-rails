1e56a3240fab8fb0343df069bf070d3c160dbd09
Evgeny Kuizhev <ekuizhev@gmail.com>
2019-05-14 21:34:24 +0300

    add puts for discriminant

--- Basic Ruby. Chapter 1/4.quadratic_equation.rb
+++ Basic Ruby. Chapter 1/4.quadratic_equation.rb
@@ -12,10 +12,12 @@ D = b**2 - 4*a*c
 if D > 0
   x1 = (-b + Math.sqrt(D)) / (2.0*a)
   x2 = (-b - Math.sqrt(D)) / (2.0*a)
+  puts "D = #{D}"
   puts "x1 = #{x1}"
   puts "x2 = #{x2}"
 elsif D == 0
   x = -b / (2.0*a)
+  puts "D = #{D}"
   puts "x = #{x}"
 else
   puts "No roots!"
